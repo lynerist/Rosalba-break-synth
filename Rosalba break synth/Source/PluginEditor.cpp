b890e082b220b4bccf77dd7689e9153dd32eb302
lynerist <leonardo.albani@studenti.unimi.it>
2021-08-04 23:20:38 +0200

    prova prova sa sa prova

--- Rosalba break synth/Source/PluginEditor.cpp
+++ Rosalba break synth/Source/PluginEditor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+    This file contains the basic framework code for a JUCE plugin editor. ciao
 
   ==============================================================================
 */
