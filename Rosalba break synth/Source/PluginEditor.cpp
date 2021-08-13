966792d051f7c7405638c2a7f9491d366efa2bdb
lynerist <leonardo.albani@studenti.unimi.it>
2021-08-13 19:19:30 +0200

    via

--- Rosalba break synth/Source/PluginEditor.cpp
+++ Rosalba break synth/Source/PluginEditor.cpp
@@ -8,7 +8,6 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-//ciao
 
 //==============================================================================
 RosalbabreaksynthAudioProcessorEditor::RosalbabreaksynthAudioProcessorEditor (RosalbabreaksynthAudioProcessor& p)
